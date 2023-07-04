df6681b012aded475b5e282ffa85db8174193345
kemal8 <kemalsubhashana8@gmail.com>
2023-07-04 14:39:33 +0530

    new user account funtion

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_passwoed_hash
+from . import db   ##means from __init__.py import db
 
 auth = Blueprint('auth',__name__)
 
@@ -16,14 +19,14 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         if len(email) < 4:
             # pass
             flash('Email must be greater than 3 characters.', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             # pass
             flash('First name must be greater than 1 characters.', category='error')
         elif password1 != password2:
@@ -35,6 +38,12 @@ def sign_up():
         else:
             #USER CREATE IN DB
             # pass
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account Created!', category='success')
+            return redirect(url_for('view.home'))
+
+
     return render_template("sign_up.html")
 
